15fe98dc20d1670f5b2b924e6d597613e5f8546c
Shunki <tkmnet@users.noreply.github.com>
2016-03-21 15:10:02 +0900

    Update Main.java

--- src/main/java/adf/Main.java
+++ src/main/java/adf/Main.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class Main
 {
-    public static final String VERSION_CODE = "2.0.9";
+    public static final String VERSION_CODE = "2.0.5";
     public static void main(String... args)
     {
         System.out.println("[ RCRS ADK Framework Version " + VERSION_CODE + " ]\n");
