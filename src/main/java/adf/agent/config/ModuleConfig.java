da6a4b04ae0bd05b19d2d143438ef2ec3fab08eb
Shunki TAKAMI(takamin@sylph.maslab) <tkmnet@users.noreply.github.com>
2016-05-30 15:55:28 +0900

    modify ModuleConfig

--- src/main/java/adf/agent/config/ModuleConfig.java
+++ src/main/java/adf/agent/config/ModuleConfig.java
@@ -2,8 +2,12 @@ package adf.agent.config;
 
 import rescuecore2.config.Config;
 
+import java.io.File;
+
 public class ModuleConfig extends Config
 {
+    public static final String DEFAULT_CONFIG_FILE_NAME = "config" +File.separator + "module.cfg";
+
     public ModuleConfig(String fileName)
     {
 
