43d677b99141fd54d7614e7ca8b0d6996b2fae48
Shunki TAKAMI(takamin@sylph.maslab) <tkmnet@users.noreply.github.com>
2016-06-13 18:41:54 +0900

    displaying the build time

--- src/main/java/adf/Main.java
+++ src/main/java/adf/Main.java
@@ -3,9 +3,13 @@ package adf;
 import adf.launcher.AgentLauncher;
 import adf.launcher.LaunchSupporter;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 
 public class Main
 {
@@ -13,7 +17,7 @@ public class Main
 
     public static void main(String... args)
     {
-        System.out.println("[ RCRS ADF Version " + VERSION_CODE + " ]\n");
+        System.out.println("[ RCRS ADF Version " + VERSION_CODE + " (build " + getTimestamp() + ") ]\n");
         List<String> launcherArguments = new ArrayList<>();
         launcherArguments.addAll(Arrays.asList(args));
         LaunchSupporter.delegate(launcherArguments);
@@ -26,5 +30,27 @@ public class Main
             System.out.println("[ERROR ] Loader not found.");
             e.printStackTrace();
         }
+    }
+
+    private static String getTimestamp()
+    {
+        String buildTimestamp = "null";
+
+        Class clazz = Main.class;
+        String className = clazz.getSimpleName() + ".class";
+        String classPath = clazz.getResource(className).toString();
+        if (!classPath.startsWith("jar"))
+        { return buildTimestamp; }
+
+        String manifestPath = classPath.substring(0, classPath.lastIndexOf("!") + 1) + "/META-INF/MANIFEST.MF";
+        try
+        {
+            Manifest manifest = new Manifest(new URL(manifestPath).openStream());
+            Attributes attributes = manifest.getMainAttributes();
+            buildTimestamp = attributes.getValue("Build-Timestamp");
+        }
+        catch (IOException e) { }
+
+        return buildTimestamp;
     }
 }
