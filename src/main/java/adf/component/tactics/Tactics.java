ab921a2b300e91305157563f0fb1e495d021b295
Shunki Takami <tkmnet@users.noreply.github.com>
2017-06-12 17:33:23 +0900

    Update: Tacrics

--- src/main/java/adf/component/tactics/Tactics.java
+++ src/main/java/adf/component/tactics/Tactics.java
@@ -8,6 +8,7 @@ import adf.agent.precompute.PrecomputeData;
 import adf.agent.info.ScenarioInfo;
 import adf.agent.info.WorldInfo;
 import adf.agent.action.Action;
+import adf.component.extaction.ExtAction;
 import adf.component.module.AbstractModule;
 
 import javax.annotation.OverridingMethodsMustInvokeSuper;
@@ -18,6 +19,7 @@ public abstract class Tactics
 {
 	private Tactics parentTactics;
 	private List<AbstractModule> modules = new ArrayList<>();
+    private List<ExtAction> modulesExtAction = new ArrayList<>();
 
 	public Tactics(Tactics parent)
 	{
@@ -29,59 +31,82 @@ public abstract class Tactics
 		this(null);
 	}
 
-
-	protected void registerModule(AbstractModule module)
-	{
-		modules.add(module);
-	}
-
-	protected boolean unregisterModule(AbstractModule module)
-	{
-		return modules.remove(module);
-    }
-
 	abstract public void initialize(AgentInfo agentInfo, WorldInfo worldInfo, ScenarioInfo scenarioInfo, ModuleManager moduleManager, MessageManager messageManager, DevelopData developData);
-
-	@OverridingMethodsMustInvokeSuper
-	public void precompute(AgentInfo agentInfo, WorldInfo worldInfo, ScenarioInfo scenarioInfo, ModuleManager moduleManager, PrecomputeData precomputeData, DevelopData developData)
-	{
-		for (AbstractModule module : modules)
-		{
-			module.precompute(precomputeData);
-		}
-	}
-
-	@OverridingMethodsMustInvokeSuper
-	public void resume(AgentInfo agentInfo, WorldInfo worldInfo, ScenarioInfo scenarioInfo, ModuleManager moduleManager, PrecomputeData precomputeData, DevelopData developData)
-	{
-		for (AbstractModule module : modules)
-		{
-			module.resume(precomputeData);
-		}
-	}
-
-	@OverridingMethodsMustInvokeSuper
-	public void preparate(AgentInfo agentInfo, WorldInfo worldInfo, ScenarioInfo scenarioInfo, ModuleManager moduleManager, DevelopData developData)
-	{
-		for (AbstractModule module : modules)
-		{
-			module.preparate();
-		}
-	}
-
-	@OverridingMethodsMustInvokeSuper
-	public Action think(AgentInfo agentInfo, WorldInfo worldInfo, ScenarioInfo scenarioInfo, ModuleManager moduleManager, MessageManager messageManager, DevelopData developData)
-	{
-		for (AbstractModule module : modules)
-		{
-			module.updateInfo(messageManager);
-		}
-
-		return null;
-	}
+	abstract public void precompute(AgentInfo agentInfo, WorldInfo worldInfo, ScenarioInfo scenarioInfo, ModuleManager moduleManager, PrecomputeData precomputeData, DevelopData developData);
+	abstract public void resume(AgentInfo agentInfo, WorldInfo worldInfo, ScenarioInfo scenarioInfo, ModuleManager moduleManager, PrecomputeData precomputeData, DevelopData developData);
+	abstract public void preparate(AgentInfo agentInfo, WorldInfo worldInfo, ScenarioInfo scenarioInfo, ModuleManager moduleManager, DevelopData developData);
+	abstract public Action think(AgentInfo agentInfo, WorldInfo worldInfo, ScenarioInfo scenarioInfo, ModuleManager moduleManager, MessageManager messageManager, DevelopData developData);
 
 	public Tactics getParentTactics()
 	{
 		return parentTactics;
 	}
+
+    protected void registerModule(AbstractModule module)
+    {
+        modules.add(module);
+    }
+
+    protected boolean unregisterModule(AbstractModule module)
+    {
+        return modules.remove(module);
+    }
+
+    protected void registerModule(ExtAction module)
+    {
+        modulesExtAction.add(module);
+    }
+
+    protected boolean unregisterModule(ExtAction module)
+    {
+        return modulesExtAction.remove(module);
+    }
+
+    protected void modulesPrecompute(PrecomputeData precomputeData)
+    {
+        for (AbstractModule module : modules)
+        {
+            module.precompute(precomputeData);
+        }
+        for (ExtAction module : modulesExtAction)
+        {
+            module.precompute(precomputeData);
+        }
+    }
+
+    protected void modulesResume(PrecomputeData precomputeData)
+    {
+        for (AbstractModule module : modules)
+        {
+            module.resume(precomputeData);
+        }
+        for (ExtAction module : modulesExtAction)
+        {
+            module.resume(precomputeData);
+        }
+    }
+
+    protected void modulesPreparate()
+    {
+        for (AbstractModule module : modules)
+        {
+            module.preparate();
+        }
+        for (ExtAction module : modulesExtAction)
+        {
+            module.preparate();
+        }
+    }
+
+    protected void modulesUpdateInfo(MessageManager messageManager)
+    {
+        for (AbstractModule module : modules)
+        {
+            module.updateInfo(messageManager);
+        }
+        for (ExtAction module : modulesExtAction)
+        {
+            module.updateInfo(messageManager);
+        }
+    }
 }
